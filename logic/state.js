3671a04ffe700d47af692eb4a6a40587473cb864
Pontus-Karlsson96 <143487143+Pontus-Karlsson96@users.noreply.github.com>
2024-05-06 15:59:45 +0200

    Started stateHandler
    planning in seperate document

---
+++ logic/state.js
@@ -0,0 +1,15 @@
+const STATE = {};
+
+function stateHandler() {
+
+    //GET user information
+
+    //POST new user + favourite function
+
+    //PATCH change theme/colour of user
+
+    //DELETE remove favourite/user
+
+
+
+};
