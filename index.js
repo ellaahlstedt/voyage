75884d8ff4da31b3d310dbe9ee7a4a3395b85a6d
Pontus-Karlsson96 <143487143+Pontus-Karlsson96@users.noreply.github.com>
2024-05-10 14:26:00 +0200

    API
    Created user.php and destinations.php
    
    user.php handles login etc
    destination.php handles GET for objects and liked/been POST/DELETE.
    
    Included tests in comments.
    
    To-do: Token requirements
    Specify errors.

--- index.js
+++ index.js
@@ -2,3 +2,78 @@ const wrapper = document.querySelector("#wrapper");
 renderNav(wrapper, "Regions");
 renderHeader(wrapper, "Regions");
 renderFooter(wrapper);
+
+
+/*async function adam() {
+    try {
+        const response = await fetch("./logic/users.php", {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({
+                id: "1"
+            })
+        });
+        console.log(response);
+        const resource = await response.json();
+        console.log(resource);
+    } catch (e) {
+        alert("error");
+    }
+}
+adam();*/
+
+/*async function adam() {
+    try {
+        const response = await fetch("./logic/destinations.php", {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                userId: "1",
+                field: "been",
+                id: "10602"
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const resource = await response.json(); // Get response text
+        console.log("Response Text:", response); // Log response text
+
+        console.log(resource);
+
+    } catch (error) {
+        console.error("Error:", error.message);
+        alert("Error occurred. Please check console for details.");
+    }
+}
+
+adam();*/
+
+
+
+/*
+GET test for destinations. change id in response to find your destination. Works for regions, country, city.
+
+async function adam() {
+    try {
+        const response = await fetch("./logic/destinations.php?id=1", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const resource = await response.json();
+        console.log(resource);
+    } catch (error) {
+        console.error("Error:", error.message);
+        alert("Error occurred. Please check console for details.");
+    }
+}
+
+adam();*/
