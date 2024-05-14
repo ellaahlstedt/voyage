3ac6ef5dba157faa92d875efe4b3e75b840f0aaf
Johanna <johanna.hallinger99@gmail.com>
2024-05-14 11:03:30 +0200

    linked logicfiles

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 "use strict";
 
-function renderRegionsCon (parent) {
+function renderRegionsCon (parent, destinations) {
     let regionsCon = document.createElement("div");
     regionsCon.id = "regionsCon";
     parent.appendChild(regionsCon);
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < destinations.length; i++) {
+        let destination = destinations[i];
+
         let regionsItem = document.createElement("div");
         regionsItem.classList = "regionsItem";
         regionsItem.style.backgroundImage = "url('images/cyprus.jpeg')"  // Ska ändras la bara till för det visuella nu 
 
         let regionsText = document.createElement("h1");
         regionsText.classList = "regionsText";
-        regionsText.textContent = "REGIONS"
+        regionsText.textContent = destination.region;
         
         regionsItem.appendChild(regionsText);
         regionsCon.appendChild(regionsItem);
@@ -20,9 +22,11 @@ function renderRegionsCon (parent) {
 }
 
 const wrapper = document.querySelector("#wrapper");
+const destinations = state_handler.get("destinations");
+console.log(destinations);
 
 renderNav(wrapper, "Regions");
 renderHeader(wrapper, "Regions");
 
-renderRegionsCon(wrapper);
+renderRegionsCon(wrapper, destinations);
 renderFooter(wrapper);
