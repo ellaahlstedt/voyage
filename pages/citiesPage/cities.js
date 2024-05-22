62917c5d16881a0c9b4de73d721bbddf9e48ddd8
Johanna <johanna.hallinger99@gmail.com>
2024-05-22 14:06:23 +0200

    Changed renderCitiesPage function

--- pages/citiesPage/cities.js
+++ pages/citiesPage/cities.js
@@ -6,25 +6,55 @@ if (window.localStorage.getItem("username") === null || window.localStorage.getI
 
 async function renderCitiesPage(parent) {
     
-    const citiesCon = document.createElement('div');
+    const citiesCon = document.createElement("div");
     citiesCon.id = "citiesCon";
 
-    renderNav(parent, 'Cities');
-    renderHeader(parent, 'Cities');
+    renderNav(parent, "Cities");
+    renderHeader(parent, "Cities");
     parent.appendChild(citiesCon);
 
-    const destinations = await fetch_handler("../../logic/destinations.php");
-    const url = window.location.href;
-    let countryParameter = null;
-    
-    if (url.includes("country=")) {
-        countryParameter = url.split("country=")[1].replace("%20", " "); // Adams kod
+    const allCities = await fetch_handler("../../logic/destinations.php?type=city");
+    const allCountries = await fetch_handler("../../logic/destinations.php?type=country");
+    const allRegions = await fetch_handler("../../logic/destinations.php?type=region");
+
+    allCities.sort(sortCountriesOrCities);
+
+    let allCityImages = [];
+
+    for (const region of allRegions) {
+        let regionImageUrl = region.regionImage;
+        let regionName = regionImageUrl.split("../images/")[1].replace(".jpeg", "");
+        
+        for (let i = 1; i < 20; i++) {
+            let cityImage = `../../images/${regionName}${i}.jpeg`;
+            allCityImages.push(cityImage);
+        }
     }
     
+    let currentAmount = 0;
+    let citiesAmount = [];
+    for(let i = 0; i < 50; i++) {
+        citiesAmount.push(allCities[i]);
+        currentAmount = i;
+    }
+    
+    renderListItem(citiesCon, citiesAmount, allCityImages);
+
+    const buttonDiv = document.createElement("div");
+    buttonDiv.id = "buttonDiv";
+    parent.appendChild(buttonDiv);
+    const pageButton = document.createElement("button");
+    pageButton.textContent = "SHOW MORE";
+    buttonDiv.appendChild(pageButton);
+
+    pageButton.addEventListener("click", function test() {
+        showMore(allCities, allCityImages, citiesCon);
+    });
+
+    /*
     if (countryParameter) {
         
         const filteredCountry = getDestinationsInRegionOrCountry(destinations, countryParameter, "country");
-        filteredCountry.cities.sort(sortCountriesOrCities);
         
         for (const destination of destinations) {
             for (const country of destination.countries) {
@@ -34,48 +64,9 @@ async function renderCitiesPage(parent) {
             }
         }
         
-    } else {
-        const allCities = [];
-
-        for (const destination of destinations) {
-            for (const country of destination.countries) {
-                for (const city of country.cities) {
-                    allCities.push(city);
-                }
-            }
-        }
-
-        let allCityImages = [];
-
-        for (const destination of destinations) {
-            for (const cityImage of destination.cityImages) {
-                allCityImages.push(cityImage);
-            }
-        }
-
-        allCities.sort(sortCountriesOrCities);
-        
-        let currentAmount = 0;
-        let citiesAmount = [];
-        for(let i = 0; i < 50; i++) {
-            citiesAmount.push(allCities[i]);
-            currentAmount = i;
-        }
-        
-        renderListItem(citiesCon, citiesAmount, allCityImages);
-        console.log(citiesCon)
-
-        const buttonDiv = document.createElement("div");
-        buttonDiv.id = "buttonDiv";
-        parent.appendChild(buttonDiv);
-        const pageButton = document.createElement("button");
-        pageButton.textContent = "SHOW MORE";
-        buttonDiv.appendChild(pageButton);
-
-        pageButton.addEventListener("click", function test() {
-            showMore(allCities, allCityImages, citiesCon);
-        });
     }
+    */
+
     renderFooter(parent);
 }
 
